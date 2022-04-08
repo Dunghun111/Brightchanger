50f6409362707d8533eac53cc14ae65f06bff04d
Dunghun111 <88573557+Dunghun111@users.noreply.github.com>
2022-04-09 00:53:17 +0700

    Adding and fixing xd
    xd

--- ChangeBrighter.py
+++ ChangeBrighter.py
@@ -6,10 +6,14 @@ def Change_Bright_HW(monitor_driver, brightness_value):
     b_file.write(f'{brightness_value}')
     b_file.close()
 
-# Use xrandr to change gamma and "Software Brightness"
+# Use xrandr to change gamma (I mean brightness)
 def Change_Bright_Gamma(brigntness_level, display):
     os.system(f'xrandr --output {display} --brightness {brigntness_level}')
 
 # Use xrandr to change r,g,b color
 def Change_RGB_Color(r, g, b, display):
     os.system(f'xrandr --output {display} --gamma {r}:{g}:{b}')
+
+# Special layout
+def Special_layout(brigntness_level, r, g, b, display)
+    os.system(f'xrandr --output {display} --gamma {r}:{g}:{b} --brightness {brigntness_level}')
